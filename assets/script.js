f030a9af48bcf101e86e911c18c6c35d7be1884d
uraniumrainbow <uraniumrainbow@gmail.com>
2023-07-26 10:31:04 -0600

    Initial commit + global variables

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//global variables
+var $SearchBtn = $("#SearchBtn");
+var $ClearBtn = $("#Clear");
+var input = document.querySelector("#search-city");
+var cityName = document.querySelector("#cityName");
+var temp = document.querySelector("#temp");
+var wind = document.querySelector("#wind");
+var humidity = document.querySelector("#humidity");
+let createImg = document.createElement("img");
